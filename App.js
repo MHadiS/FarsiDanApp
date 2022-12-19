6f98c6c33d4c72d313bc4c95c81916753e12477f
MHadiS <hadishakib6561@gmail.com>
2022-12-19 17:36:20 +0330

    add navigation to app

---
+++ App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator()
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="questions" component={QuestionsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
